f3e39b33f3effc360c1869164774982b6b3f4bbf
DorBGU <dorgreen@post.bgu.ac.il>
2018-01-01 20:18:54 +0200

    Delete init_ros.sh

--- init_ros.sh
+++
@@ -1,1 +0,0 @@
-roslaunch robotican_komodo komodo.launch gazebo:=true world_name:=/users/studs/bsc/2016/kochg/catkin_ws/src/stopper/worlds/assg2_world.sdf lidar:=true front_camera:=true depth_camera:=true
